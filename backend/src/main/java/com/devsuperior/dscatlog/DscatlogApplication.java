91ca68ecd323529f77beb0fc2066c14a2bb5369d
Vinicius Miranda <vini.s.miranda@hotmail.com>
2022-05-20 00:54:44 -0300

    backend project created

---
+++ backend/src/main/java/com/devsuperior/dscatlog/DscatlogApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatlogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscatlog/DscatlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
